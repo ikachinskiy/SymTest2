47db5aa4103ad8070aea47c809631f5cf0cc66ac
smartnet <ivan@kachinskiy.ru>
2018-03-23 16:54:17 +0300

    Опыты с PHPUnit по KNP - начали с установки PHPUnit 7.x локально в проекте

---
+++ tests/SNG/MenuBundle/Menu/MenuTests.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: smartnet
+ * Date: 23.03.18
+ * Time: 16:37
+ */
+declare(strict_types=1);
+
+
+namespace Tests\SNG\MenuBundle\Menu;
+
+use PHPUnit\Framework\TestCase;
+
+class MenuTests extends TestCase
+{
+
+}
